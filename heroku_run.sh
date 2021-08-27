2ecaacace19ed734306bc80a840c55a523cadf08
Lucas Vieira <lucasvieira@protonmail.com>
2021-08-27 14:25:50 -0300

    Modifica uso do Diesel no Heroku

--- heroku_run.sh
+++ heroku_run.sh
@@ -2,6 +2,5 @@
 rm .env
 # DATABASE_URL definido via configuração externa
 export ROCKET_KEEP_ALIVE=0
-cargo install diesel_cli --no-default-features --features postgres && diesel migration run
 exec ./target/release/minerva
 
