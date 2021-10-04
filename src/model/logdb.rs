6f93141c4ad110250345ef3f5e5aa23b3d4655e7
Lucas Vieira <lucasvieira@protonmail.com>
2021-10-03 23:02:08 -0300

    Adiciona documentação para model de log e login.

--- src/model/logdb.rs
+++ src/model/logdb.rs
@@ -14,6 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//! Utilitários de modelagem de entradas de log para banco de dados e regras de
+//! negócio.
+//! 
+//! Este módulo define estruturas para criação e recuperação de logs no sistema.
+//! Os logs serão sempre armazenados na tabela `logdb`.
+
 use super::enum_error::EnumError;
 use super::schema::logdb;
 use chrono::DateTime;
@@ -23,6 +29,11 @@ use num_derive::FromPrimitive;
 use serde::Serialize;
 use serde_repr::Serialize_repr;
 
+/// Representa um tipo de operação de modificação de estado nas tabelas do banco
+/// de dados.
+/// 
+/// Este tipo de operação deverá ser armazenado para descrever a natureza da
+/// operação realizada na tabela a ela relacionada.
 #[derive(
     FromPrimitive,
     ToPrimitive,
@@ -41,27 +52,53 @@ use serde_repr::Serialize_repr;
 #[error_type = "EnumError"]
 #[repr(i16)]
 pub enum DBOperacao {
+    /// Operação de inserção em uma tabela do banco de dados.
     Insercao = 0,
+    /// Operação de alteração de um registro em uma tabela do banco de dados.
     Alteracao = 1,
+    /// Operação de remoção de um registro em uma tabela do banco de dados.
     Remocao = 2,
 }
 
+/// Representa um único registro de log para uma operação no banco de dados.
+/// 
+/// O registro armazena dados relacionados a uma única operação de natureza
+/// específica, realizada por um usuário específico e em uma tabela específica
+/// do banco de dados.
 #[derive(Queryable, Serialize, Clone)]
 pub struct LogDB {
+    /// Id do registro de log no banco de dados.
     pub id: i32,
+    /// Tabela onde a operação se deu.
     pub tabela: String,
+    /// Usuário que realizou a operação.
     pub usuario: String,
+    /// Natureza da operação realizada.
     pub operacao: DBOperacao,
+    /// Data e hora de registro do log da operação.
     pub datahora: DateTime<chrono::Utc>,
+    /// Descrição da operação efetuada. Não é necessário informar.
     pub descricao: Option<String>,
 }
 
+/// Representa uma estrutura de inserção de um novo registro de log de uma
+/// operação no banco de dados.
 #[derive(Insertable)]
 #[table_name = "logdb"]
 pub struct NovoLogDB {
+    /// Tabela onde a operação se deu.
+    /// Veja [`LogDB::tabela`].
     pub tabela: String,
+    /// Usuário que realizou a operação.
+    /// Veja [`LogDB::usuario`].
     pub usuario: String,
+    /// Natureza da operação realizada.
+    /// Veja [`LogDB::operacao`].
     pub operacao: DBOperacao,
+    /// Data e hora de registro do log da operação.
+    /// Veja [`LogDB::datahora`].
     pub datahora: DateTime<chrono::Utc>,
+    /// Descrição opcional da operação efetuada.
+    /// Veja [`LogDB::descricao`].
     pub descricao: Option<String>,
 }
--- src/model/login.rs
+++ src/model/login.rs
@@ -14,17 +14,66 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//! Utilitários de modelagem de realização de login de um usuário.
+//! 
+//! Os utilitários deste módulo compreendem estruturas utilizadas para realizar
+//! o login de um usuário no sistema, sendo especialmente utilizadas para
+//! tráfego de dados de login no mesmo.
+//! 
+//! Para informações sobre dados de um usuário, veja [`usuario`][`super::usuario`].
+
 use serde::{Deserialize, Serialize};
 
+/// Representa dados de login recebidos através de uma requisição `POST` na rota
+/// de login. Os dados textuais desta requisição deverão ser referências diretas
+/// aos dados recebidos, para evitar cópias, já que estes dados são sensíveis.
+/// 
+/// É essencial que os dados sejam repassados através de uma conexão encriptada
+/// com SSL, para maior segurança.
+/// 
+/// Os dados de login são esperados no seguinte formato, em JSON:
+/// 
+/// ```json
+/// {
+///   "login": "admin",
+///   "senha": "admin"
+/// }
+/// ```
 #[derive(Deserialize)]
 pub struct LoginData<'r> {
+    /// Login do usuário tentando realizar login.
     pub login: &'r str,
+    /// Senha do usuário tentando realizar login, repassada em texto plano.
     pub senha: &'r str,
 }
 
+/// Representa dados de login a serem respondidos, em caso de um login
+/// bem-sucedido.
+/// 
+/// Estes dados compreendem alguns dados do usuário logado e um JWT como token
+/// de acesso, que será retornado para uso como Bearer Token em requisições
+/// subsequentes do usuário.
+/// 
+/// A resposta será em formato JSON, como mostrado no exemplo:
+/// 
+/// ```json
+/// {
+///   "id": 1,
+///   "login": "admin",
+///   "token": "..."
+/// }
+/// ```
+/// 
+/// Note que o campo `token` será um JSON Web Token apropriadamente gerado, no
+/// lugar das reticências. Para maiores informações sobre a geração deste token,
+/// veja o módulo [`auth`][`crate::auth`].
 #[derive(Serialize)]
 pub struct LoginResponse {
+    /// Id do usuário logado no banco de dados.
     pub id: i32,
+    /// Login do usuário logado no sistema.
     pub login: String,
+    /// JSON Web Token para uso como Bearer Token de autenticação em
+    /// requisições subsequentes.
     pub token: String,
 }
